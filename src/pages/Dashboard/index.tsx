849113b3a6f8d3554e8b8391f44029e43ee04ed6
LPX55 <lpx@evmosdao.org>
2024-01-15 20:37:44 +0000

    chore: lint

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { Trace } from '@uniswap/analytics'
 import { InterfacePageName } from '@uniswap/analytics-events'
-import { MAX_WIDTH_MEDIA_BREAKPOINT, MEDIUM_MEDIA_BREAKPOINT } from 'components/Tokens/constants'
 import { filterStringAtom } from 'components/Tokens/state'
 import { useResetAtom } from 'jotai/utils'
 import { useEffect } from 'react'
@@ -20,44 +19,7 @@ const ExploreContainer = styled.div`
     padding-top: 20px;
   }
 `
-const TitleContainer = styled.div`
-  margin-bottom: 32px;
-  max-width: ${MAX_WIDTH_MEDIA_BREAKPOINT};
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-`
-const FiltersContainer = styled.div`
-  display: flex;
-  gap: 8px;
-  height: 40px;
 
-  @media only screen and (max-width: ${MEDIUM_MEDIA_BREAKPOINT}) {
-    order: 2;
-  }
-`
-const SearchContainer = styled(FiltersContainer)`
-  margin-left: 8px;
-  width: 100%;
-
-  @media only screen and (max-width: ${MEDIUM_MEDIA_BREAKPOINT}) {
-    margin: 0px;
-    order: 1;
-  }
-`
-const FiltersWrapper = styled.div`
-  display: flex;
-  max-width: ${MAX_WIDTH_MEDIA_BREAKPOINT};
-  margin: 0 auto;
-  margin-bottom: 20px;
-  color: ${({ theme }) => theme.textTertiary};
-  flex-direction: row;
-
-  @media only screen and (max-width: ${MEDIUM_MEDIA_BREAKPOINT}) {
-    flex-direction: column;
-    gap: 8px;
-  }
-`
 const EmbedContainer = styled.div`
   display: flex;
   flex-direction: column;
